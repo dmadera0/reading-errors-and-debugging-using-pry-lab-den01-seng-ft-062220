8c63f18fc76c966dea74aa9f7e4a9c03878c69b6
Daniel Madera <dmadera0@gmail.com>
2020-06-09 03:53:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,8 +2,8 @@
 require 'pry'
 
 def start_game(player1, player2)
-  player1 =="Rocky"
-  player2 =="Bullwinkle"
+  player1 == Rocky
+  player2 == Bullwinkle
   "Hello #{player1} & #{playar2}"
 end
 
