d7e35c445baa4fab4017887107d3ae0eb488cd8b
Daniel Madera <dmadera0@gmail.com>
2020-06-09 03:41:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,13 +1,13 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  player1 = "Rocky"
-  player2 = "Bullwinkle"
+  player1 == "Rocky"
+  player2 == "Bullwinkle"
   "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player1, player2)
-  player1 = "Jay-z"
-  player2 = "Beyonce"
+  player1 == "Jay-z"
+  player2 == "Beyonce"
   "#{player1} is better than #{player2}"
 end
