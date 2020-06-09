458dc23ee750a7c5694299916b092d1f37c1da31
Daniel Madera <dmadera0@gmail.com>
2020-06-09 04:12:18 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,3 @@
-# don't forget to add: require 'pry'
-
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -10,7 +8,11 @@ def state_log(star_date)
 end
 
 def crew
-  []
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+end
+
+def greet_crew (crew)
+  crew.each {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
